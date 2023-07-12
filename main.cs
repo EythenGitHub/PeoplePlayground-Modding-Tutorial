7b2d5cc9e0018e894054dccc7022d234c5951fb4
Eythen - GuestGamez <117233204+EythenGitHub@users.noreply.github.com>
2023-07-12 10:45:01 -0500

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System.Linq;
+using TMPro;
+using UnityEngine.Events;
+
+
+//BASE SCRIPT
+namespace Mod
+{
+    public class Mod
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
